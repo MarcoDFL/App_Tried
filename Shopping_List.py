cc515a61874bae99d75272b3f6a3e8db0a491a34
unknown <marcodefrancesco98@gmail.com>
2019-03-22 04:19:18 +0000

    Added color red for unimplemented function message and added deleting items function

--- Shopping_List.py
+++ Shopping_List.py
@@ -1,4 +1,8 @@
-import os
+import sys
+import mmap
+from colorama import Fore, Back, Style
+from colorama import init
+init()
 
 
 # Adds a Welcome Message
@@ -26,13 +30,15 @@ def menu():
         print("")
         choice_two()
 
+    elif choice == 3:
+        print("")
+        choice_three()
     else:
-        print(chr(27) + '[2j')
-        print('\033c')
-        print('\x1bc')
-        print("==================================")
-        print('\x1b[1;30;41m' + 'Feature not implemented yet!' + '\x1b[0m')
-        print("==================================")
+        print(Fore.RED)
+        print("======================================")
+        print('Warning: Function not implemented yet!')
+        print("======================================")
+        print(Style.RESET_ALL)
         menu()
 
 
@@ -40,13 +46,13 @@ def menu():
 def shopping_list():
     shopping_list_one = []  # Creates an empty shopping list
 
-    item = raw_input("Please enter your first item: ")  # Asks user for their first item
+    item = input("Please enter your first item: ")  # Asks user for their first item
     shopping_list_one.append(item)  # Adds item into the shopping list
     print("")
     print(item + " has been added to your Shopping List.")  # informs user their item has been added to the lists
     print("")
-    while (item not in "done"):  # Checks that user is not done adding items
-        item = raw_input("Enter your next item: ")  # Asks user for their next item
+    while item not in "done":  # Checks that user is not done adding items
+        item = input("Enter your next item: ")  # Asks user for their next item
         shopping_list_one.append(item)  # Adds item to the list
         print("")  # Whitespace
         print(item + " has been added to your Shopping List.")  # informs user their item has been added to the lists
@@ -64,6 +70,7 @@ def shopping_list():
         outf.write("\n")
     outf.close()
 
+
 def choice_two():
     f = open('test.txt', 'r')
     message = f.read()
@@ -71,7 +78,20 @@ def choice_two():
     f.close()
 
 
+def choice_three():
+    f = open('test.txt', 'r')
+    print(f.read())
+    f.close()
+    remove: str = input("Which item would you like to remove?: ")
 
+    with open("test.txt", "r") as f:
+        lines = f.readlines()
+
+        with open("test.txt", "w") as f:
+            for line in lines:
+                if line.rstrip("\n") != remove:
+                    f.write(line)
+                    print(remove + " has been removed from your shopping list!")
 
 
 greeting()  # Calls function "greeting"
